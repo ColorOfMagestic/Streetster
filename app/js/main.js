a37c29c22819cfdd994cb42aca54c83199e4e377
Павел <Stem.09@mail.ru>
2021-03-20 21:20:38 +0500

    completing the entire page

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,44 @@
 
     initialSlide: 1,
 
-    centeredSlides: true
-  
+    centeredSlides: true,
+
+    loop: true
    });
+
+   new Swiper('.news-slider', {
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+
+    spaceBetween: 42,
+
+    loop: true,
+  });
+
+
+
+
+
+let nav = document.querySelector('.header-menu');
+let btnToggle = document.querySelector('.burger-menu');
+
+btnToggle.onclick = function () {
+  nav.classList.toggle('header-menu--active');
+  nav.classList.toggle('header-menu--closed');
+  btnToggle.classList.toggle('burger-menu--active');
+  btnToggle.classList.toggle('burger-menu');
+
+};
+
+let card = document.querySelector('.cart_block');
+let btnToggleCard = document.querySelector('.cart');
+
+btnToggleCard.onclick = function () {
+  card.classList.toggle('cart_block--active');
+  card.classList.toggle('cart_block--closed');
+};
   
